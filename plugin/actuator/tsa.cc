cbe5ec2125f1ff9c202cfa1aa007937c495dd6d7
ralphieraccoon <djcrosby@f2s.com>
2026-03-30 21:41:40 +0200

    Added velocity and acceleration limits.

--- plugin/actuator/tsa.cc
+++ plugin/actuator/tsa.cc
@@ -227,8 +227,8 @@ void Tsa::ActDot(const mjModel* m, mjData* d, int instance) const {
 
     Eigen::Vector2d x_dot = A + B*ctrl;
 
-    d->act_dot[state_idx] = x_dot[0];
-    d->act_dot[state_idx + 1] = x_dot[1];
+    d->act_dot[state_idx] = mju_clip(x_dot[0], -config.v_min, config.v_max);
+    d->act_dot[state_idx + 1] = mju_clip(x_dot[1], -config.a_min, config.a_max);
 
   }
 }
